62b3cb8d8644ecc56094eed2bea5ac9cb7098816
bhalla <bhalla@ncbs.res.in>
2023-12-16 16:17:43 +0530

    Change Fig8 dirs to handle mix of synth and real experiments

--- Fig8_synth/genSynthModels.bat
+++ Fig8_synth/genSynthModels.bat
@@ -3,8 +3,8 @@ exptSynth -m Models/topN_003.json -a -d Synth_EGFR_Expts
 exptSynth -sr egf 5e-8 10e-6 1000 -r intEGFR aSHC aSos aRas acraf1 aMAPK -d Synth_EGFR_Expts -m Models/topN_003.json
 exptSynth -sr aMAPK 0 0.36e-3 600 -r aSos aRas acraf1 -d Synth_EGFR_Expts -m Models/topN_003.json -b aSHC 0.4e-3
 
-hoss Config/D4_EGFR_synth_flat.json > & ! log_flat.txt
-hoss Config/D4_EGFR_synth_hoss.json > & ! log_hoss.txt
-hoss Config/D4_EGFR_synth_initScram.json > & ! log_initScram.txt
-hoss Config/D4_EGFR_synth_hossMC.json > & ! log_hossMC.txt
+hoss Config/D4_EGFR_mix_synth_flat.json > & ! log_flat.txt
+hoss Config/D4_EGFR_mix_synth_hoss.json > & ! log_hoss.txt
+hoss Config/D4_EGFR_mix_synth_initScram.json > & ! log_initScram.txt
+hoss Config/D4_EGFR_mix_synth_hossMC.json > & ! log_hossMC.txt
 
