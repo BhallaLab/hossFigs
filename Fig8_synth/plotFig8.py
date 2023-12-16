c08ffb462f594af132c3aa130fbca10122bf2195
bhalla <bhalla@ncbs.res.in>
2023-12-16 15:49:46 +0530

    Add script plotFig8.py

---
+++ Fig8_synth/plotFig8.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import re
+import numpy as np
+import dispAllExpts as dae
+
+import matplotlib.pyplot as plt
+
+bar_width = 0.35
+space_width = 0.2
+
+optFiles = ["OPT_FLAT/OPTI_000.g", "OPT_HOSS/OPTI_000.g", "OPT_INITSCRAM/topN_000.g", "OPT_HOSSMC/topN_6_000.g" ]
+origFile = "Models/D4_model_EGFR_v13b.g"
+origOptFile = "Models/OPTI_000.g"
+d3File = "Models/D3_model_MAPK_v7.json"
+synthConfigFile = "Config/D4_EGFR_synth_hoss.json"
+dataConfigFile = "Config/D4_EGFR_hoss.json"
+
+class Args( ):
+    def __init__( self, modelFile, mapFile, configFile ):
+        self.model = modelFile
+        self.map = mapFile
+        self.config = configFile
+        self.scoreFunc = "NRMS"
+        self.solver = "LSODA"
+        #self.exptDir = "Synth_EGFR_Expts"
+        self.exptDir = None
+        self.hidePlot = True
+        self.verbose = False
+        self.blocks = []
+        self.plot = ""
+
+def computeScores( configFile, modelFile ):
+    args = Args( modelFile, "Maps/D4_map_EGFR.json", configFile )
+    return dae.innerMain( args )
+
+
+def plotAllScores( ax ):
+    # Here we have to scale the flat scoring scheme to match the 
+    # hierarchical scoring. To do this we normalize to the initial value.
+    # Plotting parameters
+
+    synthInitScore = computeScores( synthConfigFile, origFile )
+    realInitScore = computeScores( dataConfigFile, origFile )
+    synthExpt2SynthScores = [ computeScores( synthConfigFile, mm ) for mm in optFiles ]
+    realExpt2SynthScores = [ computeScores( dataConfigFile, mm ) for mm in optFiles ]
+    scoreDict = {"Synthetic": synthExpt2SynthScores, 
+            "Real": realExpt2SynthScores }
+
+    categories = [key for key in scoreDict]
+
+    bar_width = 0.2
+    space_width = 0.3  # Adjust as needed
+    colors = {'Initial': 'maroon', 'Flat': 'blue', 'Hoss': 'green', 'initScram': 'red', 'hossMC': 'purple'}  # Colors for each optimization method
+    
+    # Extracting data for plotting
+    xticklabels = []
+    
+    grouped_positions = np.arange(len(categories) ) * (bar_width * len(colors) + space_width)
+    
+    for idx, cc in enumerate( categories ):
+            xticklabels.append( cc )
+            positions = grouped_positions[idx] + np.arange( len(colors) ) * bar_width
+    
+            initScore = synthInitScore if idx == 0 else realInitScore
+            ax.bar(positions[0], initScore, bar_width, align='center', 
+                alpha=0.7, ecolor='black', capsize=5, 
+                color=colors['Initial'],
+                label='Initial' if idx == 0 else "")
+            flatScore = scoreDict[cc][0]
+            ax.bar(positions[1], flatScore, bar_width, align='center', 
+                alpha=0.7, ecolor='black', capsize=5, color=colors['Flat'],
+                label='Flat' if idx == 0 else "")
+            hossScore = scoreDict[cc][1]
+            ax.bar(positions[2], hossScore, bar_width, align='center', 
+                alpha=0.7, ecolor='black', capsize=5, color=colors['Hoss'],
+                label='Hoss' if idx == 0 else "")
+            initScramScore = scoreDict[cc][2]
+            ax.bar(positions[3], initScramScore, bar_width, align='center', 
+                alpha=0.7, ecolor='black', capsize=5, 
+                color=colors['initScram'],
+                label='initScram' if idx == 0 else "")
+            hossMcScore = scoreDict[cc][3]
+            ax.bar(positions[4], hossMcScore, bar_width, align='center', 
+                alpha=0.7, ecolor='black', capsize=5, 
+                color=colors['hossMC'],
+                label='hossMC' if idx == 0 else "")
+    
+    # Adding labels and title
+    ax.set_xticks(grouped_positions + ((len(categories) - 1) / 2) * (bar_width * len(colors) + space_width))
+    ax.set_xticklabels( xticklabels, fontsize = 16 )
+    ax.set_ylabel('Optimization score', fontsize = 16)
+    ax.set_ylim( 0, 0.65 )
+    ax.yaxis.set_tick_params(labelsize=14)
+    ax.text( -0.10, 1.05, "B", fontsize = 22, weight = "bold", transform=ax.transAxes )
+    
+    # Adding legend
+    #ax.legend(loc='upper left', title='Optimization Method', frameon = False, fontsize = 14)
+    ax.legend(loc='upper left', ncol = 2, frameon = False, fontsize = 14)
+    
+def main():
+    fig, ax = plt.subplots( nrows = 2, ncols=1, figsize = (8, 12) )
+    plotAllScores( ax[0])
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
